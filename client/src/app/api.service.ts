35916846e086bdf7ef694d600b42e30f15391b97
Huor97 <huor97ka@gmail.com>
2023-10-16 00:21:00 +0200

    recuper cordonner à partir d'un api

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,15 +1,18 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ApiService {
-//   private apiUrl = 'http://localhost:8000'; // Remplacez ceci par l'URL de votre API
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'api'; // Remplacez ceci par l'URL de votre API
 
-//   constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-//   getCoordinates() {
-//     return this.http.get(`${this.apiUrl}/get_coordinates/`);
-//   }
-// }
+  getDonnees(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`);
+  }
+
+  // Ajoutez d'autres méthodes pour POST, PUT, DELETE, etc. selon les besoins
+}
